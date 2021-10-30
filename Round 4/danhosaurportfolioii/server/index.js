06bc792e51d8e191cc6caa5e4d0a73b6f3e2afad
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-10-30 16:21:11 +0200

    did i fix the server?

--- Round 4/danhosaurportfolioii/server/index.js
+++ Round 4/danhosaurportfolioii/server/index.js
@@ -1,24 +1,30 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
+const router = express.Router();
 
 const { json } = require('body-parser');
 app.use(json({ limit: '50mb' }));
 
 const cors = require('cors');
 app.use(cors());
-app.get('/', (req, res) => {
+
+router.get('/', (req, res) => {
     res.send('Hello there!');
 })
-app.use('/helloworld', require('./helloworld'));
-app.use('/api/projects', require('./routes/api/projects'));
-app.use('/api/projects/:id', require('./routes/api/projects'));
-app.use('/api/admins', require('./routes/api/admins'));
-// app.use('/postProjects', require('./runProjects'))
+
+const prefix = '/.netlifly/functions';
+app.use(prefix, router)
+app.use(`${prefix}/helloworld`, require('./helloworld'));
+app.use(`${prefix}/api/projects`, require('./routes/api/projects'));
+app.use(`${prefix}/api/projects/:id`, require('./routes/api/projects'));
+app.use(`${prefix}/api/admins`, require('./routes/api/admins'));
+// app.use(`${prefix}/postProjects`, require('./runProjects'))
 
 require('dotenv').config();
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}!`));
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => console.log(`Server started on port ${port}!`));
 
 module.exports = app;
 module.exports.port = port;
+module.exports.handler = serverless(app);
